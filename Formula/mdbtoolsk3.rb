10a5835a50460c1e9267c6987aae1034b051a139
JJ Breen <jj.breen@knowink.com>
2025-10-24 08:12:49 -0500

    The real fix for homebrew (see https://github.com/mdbtools/mdbtools/commit/11607108be12a083bc4c916947378c5c3144804d )

--- Formula/mdbtoolsk3.rb
+++ Formula/mdbtoolsk3.rb
@@ -21,8 +21,7 @@ class Mdbtoolsk3 < Formula
   end
 
   def install
-    ENV.prepend_path "PATH", "/opt/local/bin"
-    system "autoreconf", "--force", "--install", "--verbose"
+    system "autoreconf", "--force", "--install", "--verbose", "-I", "#{ENV["HOMEBREW_PREFIX"]}/share/gettext/m4"
     system "./configure", "--enable-man", "--program-suffix=-k3", *std_configure_args
     system "make", "install"
   end
